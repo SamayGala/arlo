ad6ba314736dc887d7cf4d5b9feb5d42527fb2a4
SamayGala <samay@corporatecoders.com>
2021-05-25 15:53:12 +0530

    Reverted styling & header and added Back button, redirection to home on logo click

--- client/src/components/Header.test.tsx
+++ client/src/components/Header.test.tsx
@@ -148,11 +148,17 @@ describe('Header', () => {
       })
 
       // Audit board name
-      await screen.findByText(/Audit Board #1/)
+      expect(screen.getAllByText(/Audit Board #1/).length).toBe(2)
 
       // Sign Out link available for audit board
-      const signOutButton = screen.getByRole('link', { name: 'Sign Out' })
-      expect(signOutButton).toHaveAttribute('href', '/auth/logout')
+      const userButton = screen.getByRole('button', {
+        name: /Audit Board #1/,
+      })
+      userEvent.click(userButton)
+
+      // Dropdown menu should show with log out option
+      const logOutButton = screen.getByRole('link', { name: 'Log out' })
+      expect(logOutButton).toHaveAttribute('href', '/auth/logout')
     })
   })
 
